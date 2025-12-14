70d1b80c99d3913662cb56a37fe182c980663770
m34r1k <m34r1k@naver.com>
2025-12-14 22:23:46 +0900

    10871번 X보다 작은 수 문제 해결

---
+++ Class 1/10869_사칙연산.py
@@ -0,0 +1,11 @@
+a = input()
+temp = a.split()
+print(int(temp[0]) + int(temp[1]))
+
+print(int(temp[0]) - int(temp[1]))
+
+print(int(temp[0]) * int(temp[1]))
+
+print(int(temp[0]) // int(temp[1]))
+
+print(int(temp[0]) % int(temp[1]))
