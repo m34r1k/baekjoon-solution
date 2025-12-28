28e31f996993e40f4e33fc80a842cd41de38eda9
m34r1k <m34r1k@naver.com>
2025-12-28 22:22:42 +0900

    10825번 국영수 문제 Index Error

--- Class 2/10825_국영수.py
+++ Class 2/10825_국영수.py
@@ -1,4 +1,10 @@
-repeat = input()
+n = int(input())
 scores = []
-for i in range(repeat):
+for i in range(n):
+    name_list = []
+    scores = list(input().split())
+    scores.append(name_list)
+    scores.sort(key=lambda x: [x[1], x[2], x[3], x[0]])
     
+for score in scores:
+    print(score[0])
