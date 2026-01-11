43b7fd9ab97d510fdcbc59c1aab566537cdf4b6b
m34r1k <m34r1k@naver.com>
2026-01-11 22:14:18 +0900

    1181번 단어 정렬 문제 해결

--- Unranked/1181_단어_정렬.py
+++ Unranked/1181_단어_정렬.py
@@ -4,6 +4,8 @@ for i in range(n):
     word = input()
     word_list.append((len(word), word)) #[(3, 'but'), (1, 'i'), (4, 'wont'), (8, 'hesitate'), (2, 'no'), (4, 'more'), (2, 'no'), (4, 'more'), (2, 'it'), (6, 'cannot'), (4, 'wait'), (2, 'im'), (5, 'yours')]
     
-    word_list.sort()
-    for i in word_list:
-        print(i[1])
+    #중복 제거하기
+    word_list.sort(key=lambda x: x[0])
+    unique_word_list = set(word_list)
+for i in unique_word_list:
+    print(i[1])
