1b0f7b51110a1f43844bf80ad3ec4c56ed90bfe9
m34r1k <m34r1k@naver.com>
2026-01-25 22:20:36 +0900

    11652번 카드 문제 미완성

--- Unranked/1431_시리얼_번호.py
+++ Unranked/1431_시리얼_번호.py
@@ -1,16 +1,16 @@
 def get_sum(serial):
     default = 0
-    for i in serial:
-        if i.isdigit():
-            default += int(i)
+    for i in serial: 
+        if i.isdigit(): #만약 i가 숫자라면
+            default += int(i) #원래 있던 값에 더하기
     return default
     
 n = int(input())
 serials = []
 
 for i in range(n):
-    serials.append(input())
-    serials.sort(key=lambda x: (len(x), get_sum(x), x))
+    serials.append(input()) #시리얼 코드 추가
+    serials.sort(key=lambda x: (len(x), get_sum(x), x)) #조건
     
 for i in serials:
     print(i)
